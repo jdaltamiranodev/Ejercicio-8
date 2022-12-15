371c0bff7a8e979e715ca872facfd1ecc5b01d1e
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-12-15 18:38:32 -0500

    Enunciado del ejercicio:
    En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
    
    Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.

---
+++ main.py
@@ -0,0 +1,13 @@
+import operaciones_matematicas
+
+operacion = operaciones_matematicas.suma(5, 6)
+print(f"La suma es: ", operacion)
+
+operacion = operaciones_matematicas.resta(10, 6)
+print("La resta es: ", operacion)
+
+operacion = operaciones_matematicas.multiplica(4, 8)
+print("La multiplicación es: ", operacion)
+
+operacion = operaciones_matematicas.divide(25, 5)
+print("La división es: ", operacion)
